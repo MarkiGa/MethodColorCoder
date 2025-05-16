68fa8f8de26bcfc8ac4e2d1114d7685b7bc7630f
MarkiGa <73349060+MarkiGa@users.noreply.github.com>
2025-05-16 21:07:35 +0200

    changes ??

--- src/extension.ts
+++ src/extension.ts
@@ -205,13 +205,13 @@ function getWeightedHueFromString(str: string): string {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('highlighter.deactivate', () => {
+	const disposable = vscode.commands.registerCommand('MethodColorCoder.deactivate', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Deactivate!');
         activateDeactivateLogic(false);
 	});
-	const reactivate = vscode.commands.registerCommand('highlighter.reactivate', () => {
+	const reactivate = vscode.commands.registerCommand('MethodColorCoder.reactivate', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Reactivated!');
